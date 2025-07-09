8b11f5adb420c73aae8b58d378e095cef4db2c5d
Nikita <nikkitos.pershin.2000@mail.ru>
2025-07-09 20:07:08 +0500

    CRUD forms

--- catalog/views.py
+++ catalog/views.py
@@ -1,51 +1,59 @@
 # from django.http import HttpResponse
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 # from .forms import ProductForm
 
 class ProductCreateView(CreateView):
     model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:products_list")
     fields = ("name","description","stock","category","image","price")
-    success_url = reverse_lazy("catalog:catalog_list")
 
 
 class ProductUpdateView(UpdateView):
     model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
     fields = ("name", "description", "stock", "category", "image", "price")
     success_url = reverse_lazy("catalog:catalog_list")
 
 
 class ProductDeleteView(DeleteView):
     model = Product
+    template_name = "catalog/product_delete_confirm.html"
     success_url = reverse_lazy("catalog:catalog_list")
+    context_object_name = "product"
 
 class ProductListView(ListView):
     model = Product
-
+    template_name = (
+        "catalog/products_list.html"  # Имя шаблона для отображения списка продуктов
+    )
 
 class ProductDetailView(DetailView):
     model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
 
 
-class ContactDetailView(TemplateView):
-    template_name = "catalog/contacts.html"
+class ContactTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
 
-
-# class ContactTemplateView(TemplateView):
-#     template_name = 'catalog/contacts.html'
-#
-#     def get_context_data(self, **kwargs):
-#         if self.request.method == 'POST':
-#             name = self.request.POST.get('name')
-#             phone = self.request.POST.get('phone')
-#             message = self.request.POST.get('message')
-#             print(name)
-#             print(phone)
-#             print(message)
-#             return HttpResponse('Сообщение отправлено!')
+    def get_context_data(self, **kwargs):
+        if self.request.method == 'POST':
+            name = self.request.POST.get('name')
+            phone = self.request.POST.get('phone')
+            message = self.request.POST.get('message')
+            print(name)
+            print(phone)
+            print(message)
+            return HttpResponse('Сообщение отправлено!')
 
 
 # class ProductCreateView(CreateView):
