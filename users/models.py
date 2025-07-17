6c258e1cb34dd08135ea02f5e95f7c04bb7d9bcf
Nikita <nikkitos.pershin.2000@mail.ru>
2025-07-18 00:08:24 +0500

    models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField("email", unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    country = models.CharField(max_length=30, blank=True, null=True)
+    token = models.CharField(max_length=100, blank=True, null=True, verbose_name="Token")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
